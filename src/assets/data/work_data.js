28e30a8adbf7cd2c668666df7807027f3d0ea579
Hamza Khan Niazi <hamzaniazi91@gmail.com>
2018-06-15 18:09:00 +0500

    Work Changes

--- src/assets/data/work_data.js
+++ src/assets/data/work_data.js
@@ -4,45 +4,75 @@ import environmindful from '../images/work/environmindful.png';
 import matchTwo from '../images/work/match_two.png';
 import portfolio from '../images/work/create_react_portfolio.png';
 
+import bcdapp from '../images/work/bcdapp1.gif'
+import Lms from '../images/work/Lms.png'
+import Highcharts  from '../images/work/Highcharts.png'
+
+import Yummy  from '../images/work/Yummy.png'
+
+import HFA  from '../images/work/HFA.png'
+
+
 export default [
     {
-        name: 'BestCityDrivers',
-        info: 'Built with a MERN Stack (MongoDB, Express, React Redux, Node.js), FlexNotes is an multi-panel note taking application that allows students to organize their multi-media study materials in one place.  Flexnotes has a binder, tab, and page navigation for organization. And each page has three panels: one for videos, slides, and notes. My role in this group project was creating the navigation, linking, routing and connecting the front end to the back end.',
-        img: flexnotes,
-        alt: 'FlexNotes',
-        codeLink: "https://github.com/conmeconte/flexnotes",
-        demoLink: "https://flexnotes.net/"
+        name: 'Best City Drivers',
+        info: 'BCD Web, Mobile Apps & BackendThe BCD app will let users easily and conveniently rent a driver. In the simplest form, that is the core essence of this project.',
+        img: bcdapp,
+        alt: 'bcdapp',
+        codeLink: "https://github.com/hamzaniazi91/spectrm-task",
+        demoLink: "https://hamzaniazi91.github.com/Portfolio",
+        
     },
     {
         name: 'Learning Management System',
         info: 'Originally developed during an API hackathon, Environmindful is an environmental dashboard using Google Maps, Google Charts, OpenWeather, AQI, Carma and News APIs. Its purpose is to promote environmental awareness around the world. Built with jQuery, Ajax, and Bootstrap.',
-        img: environmindful,    
-        alt: 'Environmindful',
+        img: Lms,    
+        alt: 'Lms',
         codeLink: "https://github.com/hamzaniazi91/LMS",
-        demoLink: "https://brucean52.github.io/Environmindful/"
+        demoLink: "https://hamzaniazi91.github.com/Portfolio"
     },
     {
-        name: 'Stock Market Highmaps',
+        name: 'Stock Market Highmaps D3.js',
         info: 'Match Two! is a comic book themed, memory match game. This project was built using jQuery, Bootstrap, scalable vector graphics (SVG), Photoshop, and Illustrator.',
-        img: matchTwo,
-        alt: 'Match Two',
-        codeLink: "https://github.com/brucean52/memory-match-js",
-        demoLink: "https://github.com/hamzaniazi91/d3.js-Highcharts-Treemap"
+        img: Highcharts,
+        alt: 'Highcharts',
+        codeLink: "https://github.com/hamzaniazi91/d3.js-Highcharts-Treemap",
+        demoLink: "http://bullandbear.com.pk/test-2/"
     },
     {   
-        name: 'Contact List',
+        name: 'Appylicious',
         info: 'Contact list is a mobile responsive technical demo using a model-view-controller (MVC) architecture, jQuery, Ajax, and Flexbox. A PHP back end was used to perform CRUD operations to a MySQL database. Contacts are sortable by name, and the app is fully mobile responsive.',
-        img: contactList,
-        alt: 'Contact List',
-        codeLink: "https://github.com/brucean52/contact_list",
-        demoLink: "http://contact-list.brucedev.net/"
-    },
-    {
-        name: 'Create React Portfolio',
-        info: 'A simple portfolio template using React, Nodemailer and Material Design.',
-        img: portfolio,
-        alt: 'Portfolio',
-        codeLink: "https://github.com/brucean52/create-react-portfolio",
+        img: Yummy,
+        alt: 'Yummy',
+        codeLink: "https://hamzaniazi91.github.com/Portfolio",
         demoLink: ""
     },
+    // {
+    //     name: 'Create React Portfolio',
+    //     info: 'A simple portfolio template using React, Nodemailer and Material Design.',
+    //     img: portfolio,
+    //     alt: 'Portfolio',
+    //     codeLink: "https://github.com/brucean52/create-react-portfolio",
+    //     demoLink: ""
+    // },
+
+    {   
+        name: 'HFA Audit Application',
+        info: 'Contact list is a mobile responsive technical demo using a model-view-controller (MVC) architecture, jQuery, Ajax, and Flexbox. A PHP back end was used to perform CRUD operations to a MySQL database. Contacts are sortable by name, and the app is fully mobile responsive.',
+        img: HFA,
+        alt: 'HFA',
+        codeLink: "https://github.com/hamzaniazi91/HFA_Audit_App",
+        demoLink: "https://hamzaniazi91.github.com/Portfolio"
+    },
+
+    // {   
+    //     name: 'Yummylicious',
+    //     info: 'Contact list is a mobile responsive technical demo using a model-view-controller (MVC) architecture, jQuery, Ajax, and Flexbox. A PHP back end was used to perform CRUD operations to a MySQL database. Contacts are sortable by name, and the app is fully mobile responsive.',
+    //     img: contactList,
+    //     alt: 'Contact List',
+    //     codeLink: "https://github.com/brucean52/contact_list",
+    //     demoLink: "http://contact-list.brucedev.net/"
+    // },
+
+
 ];
